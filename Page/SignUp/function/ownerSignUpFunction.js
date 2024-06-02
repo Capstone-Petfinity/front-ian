ee73c16945a4e9894d980f76901a1d9e29fc5161
강민재 <minijae011030@naver.com>
2024-06-03 00:24:16 +0900

    encrypt owner signup

--- Page/SignUp/function/ownerSignUpFunction.js
+++ Page/SignUp/function/ownerSignUpFunction.js
@@ -1,3 +1,5 @@
+import CryptoJS from 'react-native-crypto-js';
+
 export default async function ownerSignUpFunction({
   userId,
   password,
@@ -5,23 +7,45 @@ export default async function ownerSignUpFunction({
   phone,
   city,
 }) {
-  console.log(userId, password, name, phone, city);
+  const bodyData = JSON.stringify({
+    id: userId,
+    pw: password,
+    name: name,
+    phone_number: phone,
+    city: city,
+  });
 
+  const secretKey = 'abcdefghabcdefghabcdefghabcdefgh'; // 32 bytes key
+  const initVector = '0123456789abcdef'; // 16 bytes IV
+
+  // Convert the plain text, key, and IV to WordArrays
+  const encodedText = CryptoJS.enc.Utf8.parse(bodyData);
+  const encodedKey = CryptoJS.enc.Utf8.parse(secretKey);
+  const encodedIV = CryptoJS.enc.Utf8.parse(initVector);
+
+  // Create the options object for encryption
+  const options = {
+    iv: encodedIV,
+    mode: CryptoJS.mode.CBC,
+    padding: CryptoJS.pad.Pkcs7,
+  };
+
+  // Encrypt the data using AES encryption with PKCS7Padding and an IV
+  const encryptedData = CryptoJS.AES.encrypt(
+    encodedText,
+    encodedKey,
+    options,
+  ).toString(); // Convert CipherParams object to a base64 string
+  console.log(encryptedData);
   const result = await fetch(
     'https://capstone-petfinity.com/user/signup/parent',
     {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'text/plain',
         auth: 'bVAtkPtiVGpWuO3dWEnvr51cEb6r7oF8',
       },
-      body: JSON.stringify({
-        id: userId,
-        pw: password,
-        name: name,
-        phone_number: phone,
-        city: city,
-      }),
+      body: encryptedData,
     },
   );
 
